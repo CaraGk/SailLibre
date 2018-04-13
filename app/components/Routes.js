40906a4de9bc44f76f241292038127470f20d5a7
Pierre-François Pilou Angrand <caragk@medlibre.fr>
2018-04-13 14:51:03 +0200

    Readability

--- app/components/Routes.js
+++ app/components/Routes.js
@@ -1,5 +1,16 @@
 module.exports = [
-        {path: '/home', component: require('./Home'), meta: {title: 'SailLibre HomePage'}},
-        {path: '/converter', component: require('./Converter'), meta: {title: 'Distance converter'}},
-        {path: '*', redirect: '/home'}
+        {
+            path: '/home',
+            component: require('./Home'),
+            meta: {title: 'SailLibre HomePage'}
+        },
+        {
+            path: '/converter',
+            component: require('./Converter'),
+            meta: {title: 'Distance converter'}
+        },
+        {
+            path: '*',
+            redirect: '/home'
+        }
 ];
